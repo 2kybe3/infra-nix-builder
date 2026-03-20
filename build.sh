02a6bda81586396f5305ca06498638446098e001
2kybe3 <kybe@kybe.xyz>
2026-03-20 22:35:55 +0100

    init v2

---
+++ build.sh
@@ -0,0 +1,36 @@
+set -euo pipefail
+
+GREEN="\033[0;32m"
+NC="\033[0m"
+
+echo_cmd() {
+    echo -e "${GREEN}+ $*${NC}"
+}
+
+REPO_URL=$(echo "$json" | jq -r '.repo')
+TARGET_HOST=$(echo "$json" | jq -r '.host')
+TARGET_IP=$(echo "$json" | jq -r '.ip')
+TARGET_USER=$(echo "$json" | jq -r '.user')
+
+echo_cmd "BUILD_DIR=\$(mktemp -d /tmp/build-XXXX)"
+BUILD_DIR=$(mktemp -d /tmp/build-XXXX)
+
+echo_cmd "trap 'rm -rf \$BUILD_DIR' EXIT"
+trap 'rm -rf "$BUILD_DIR"' EXIT
+
+echo_cmd "git clone --depth 1 $REPO_URL $BUILD_DIR"
+git clone --depth 1 "$REPO_URL" "$BUILD_DIR"
+
+echo_cmd "cd $BUILD_DIR"
+cd "$BUILD_DIR"
+
+echo_cmd "export NIX_SSHOPTS='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'"
+export NIX_SSHOPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+echo_cmd "nixos-rebuild switch --flake .#$TARGET_HOST --target-host $TARGET_USER@$TARGET_IP --upgrade --sudo --verbose"
+nixos-rebuild switch \
+    --flake .#$TARGET_HOST \
+    --target-host $TARGET_USER@$TARGET_IP \
+    --upgrade \
+    --sudo \
+    --verbose
--- flake.nix
+++ flake.nix
@@ -3,11 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    sops-nix,
   }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
@@ -21,6 +27,7 @@
 
       modules = [
         ./configuration.nix
+        sops-nix.nixosModules.sops
       ];
     };
     formatter.${system} = nixpkgs.legacyPackages.${system}.alejandra;
