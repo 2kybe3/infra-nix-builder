88291d1f0d17a654518267d77f00ef94d7478f88
2kybe3 <kybe@kybe.xyz>
2026-03-27 20:31:25 +0100

    tweaks

--- configuration.nix
+++ configuration.nix
@@ -53,6 +53,7 @@ in {
     openssh.authorizedKeys = {
       keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM7irWuDZwx7ZvPSiUwBbxUxKL/7aMQmy/8oxput1bID kybe@knx"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIErsJeX2oAU856/1zRtN1OsMCDRaipUeqaV8vRqSNi6H git.kybe.xyz -> nix-builder"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDeptJ/WeiQ3wuCpkHYAfSdH5h5l6kbJFSkiM/g7pZjZ git.kybe.xyz (infra-caddy) -> nix-builder"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOzHEdANrzJkiRCD+WdoGhJieehwxuuXevLaNUPp83Ki git.kybe.xyz (infra-nix-main) -> nix-builder"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIODsU2Eyfj0bdNumTw9HW6yDukyNxNdc5q7JK7ONLeMm git.kybe.xyz (infra-nix-builder) -> nix-builder"
