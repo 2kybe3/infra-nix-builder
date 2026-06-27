bfab3efaa2701c07d8e9700164eb8d1337e1b4f6
2kybe3 <kybe@kybe.xyz>
2026-06-27 11:03:32 +0200

    nix settings

--- modules/nix.nix
+++ modules/nix.nix
@@ -1,19 +1,22 @@
 {
-  nix.settings = {
-    auto-optimise-store = true;
-    experimental-features = [
-      "pipe-operators"
-      "nix-command"
-      "flakes"
-    ];
-    max-jobs = 32;
-    substituters = [ "https://attic.kybe.xyz/main" ];
-    trusted-public-keys = [ "main:cb7V485kGP0lG7LtQ/suOgKOgtVxNXrnD6i5yCtnaMQ=" ];
-    trusted-substituters = [ "https://attic.kybe.xyz/main" ];
-    trusted-users = [
-      "root"
-      "@wheel"
-    ];
-    use-xdg-base-directories = true;
+  nix = {
+    enable = true;
+    channel.enable = false;
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "pipe-operators"
+        "nix-command"
+        "flakes"
+      ];
+      substituters = [ "https://attic.kybe.xyz/main" ];
+      trusted-public-keys = [ "main:cb7V485kGP0lG7LtQ/suOgKOgtVxNXrnD6i5yCtnaMQ=" ];
+      trusted-substituters = [ "https://attic.kybe.xyz/main" ];
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+      use-xdg-base-directories = true;
+    };
   };
 }
